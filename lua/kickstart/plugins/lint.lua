4fe3affbc21cf2be17617a5cea7df5143033f863
Jon Brown <brojonat@gmail.com>
2025-11-09 13:17:51 -0800

    remove html prettier linting

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,7 +7,6 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        html = { 'prettier' },
       }
 
       -- Configure markdownlint to ignore MD024 (duplicate heading)
